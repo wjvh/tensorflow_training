9e77ef3c927abba66a930cbb0612d03792b75d8e
Alfred <angch@tertiaryinfotech.com>
2017-06-18 22:53:12 +0800

    19062017

--- exercises/module6_1_CNN_mnist_demo.py
+++ exercises/module6_1_CNN_mnist_demo.py
@@ -3,25 +3,14 @@
 
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("mnist", one_hot = True)
+mnist = input_data.read_data_sets("mnist/", one_hot = True)
 
 n_classes = 10
 batch_size = 128
 
-x = tf.placeholder('float', [None, 784])
-y = tf.placeholder('float')
+graph = tf.Graph()
 
-keep_rate = 0.8
-keep_prob = tf.placeholder(tf.float32)
-
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME')
-
-def maxpool2d(x):
-    #size of window movement of window
-    return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
-
-def convolutional_neural_network(x):
+with graph.as_default():
     weights = {'W_conv1':tf.Variable(tf.random_normal([5,5,1,32])),
                'W_conv2':tf.Variable(tf.random_normal([5,5,32,64])),
                'W_fc':tf.Variable(tf.random_normal([7*7*64,1024])),
@@ -32,44 +21,42 @@ def convolutional_neural_network(x):
                'b_fc':tf.Variable(tf.random_normal([1024])),
                'out':tf.Variable(tf.random_normal([n_classes]))}
 
-    x = tf.reshape(x, shape=[-1, 28, 28, 1])
+    x = tf.placeholder('float', [None, 784])
+    y = tf.placeholder('float')
+    keep_prob = tf.placeholder(tf.float32)
 
-    conv1 = tf.nn.relu(conv2d(x, weights['W_conv1']) + biases['b_conv1'])
-    conv1 = maxpool2d(conv1)
+    inp = tf.reshape(x, shape=[-1, 28, 28, 1])
+
+    conv1 = tf.nn.relu(tf.nn.conv2d(inp, weights['W_conv1'], strides=[1,1,1,1], padding='SAME') + biases['b_conv1'])
+    conv1 = tf.nn.max_pool(conv1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
     
-    conv2 = tf.nn.relu(conv2d(conv1, weights['W_conv2']) + biases['b_conv2'])
-    conv2 = maxpool2d(conv2)
+    conv2 = tf.nn.relu(tf.nn.conv2d(conv1, weights['W_conv2'], strides=[1,1,1,1], padding='SAME') + biases['b_conv2'])
+    conv2 = tf.nn.max_pool(conv2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
     fc = tf.reshape(conv2,[-1, 7*7*64])
     fc = tf.nn.relu(tf.matmul(fc, weights['W_fc'])+biases['b_fc'])
-    fc = tf.nn.dropout(fc, keep_rate)
+    fc = tf.nn.dropout(fc, keep_prob)
 
     output = tf.matmul(fc, weights['out'])+biases['out']
-
-    return output
-
-def train_neural_network(x):
-    prediction = convolutional_neural_network(x)
-    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y) )
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=output, labels=y) )
     optimizer = tf.train.AdamOptimizer().minimize(cost)
-    
-    hm_epochs = 3
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
+    correct = tf.equal(tf.argmax(output, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
 
-        for epoch in range(hm_epochs):
-            epoch_loss = 0
-            for _ in range(int(mnist.train.num_examples/batch_size)):
-                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
-                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
-                epoch_loss += c
+hm_epochs = 3
+with tf.Session(graph=graph) as sess:
+    sess.run(tf.global_variables_initializer())
 
-            print('Epoch', epoch+1, 'completed out of',hm_epochs,'loss:',epoch_loss)    
-        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        acc = []
-        for i in range(100):
-            acc.append(accuracy.eval({x:mnist.test.images[(1 + (i*100)):((i+1)*100),], y:mnist.test.labels[(1 + (i*100)):((i+1)*100),]}))
-        print('Accuracy:', sess.run(tf.reduce_mean(acc)))
+    for epoch in range(hm_epochs):
+        epoch_loss = 0
+        for _ in range(int(mnist.train.num_examples/batch_size)):
+            epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+            _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y, keep_prob: 0.8})
+            epoch_loss += c
 
-train_neural_network(x)
+        print('Epoch', epoch+1, 'completed out of', hm_epochs, 'loss:', epoch_loss)
+
+    acc = []
+    for i in range(100):
+        acc.append(accuracy.eval({x: mnist.test.images[(1 + (i*100)):((i+1)*100),], y: mnist.test.labels[(1 + (i*100)):((i+1)*100),], keep_prob:1}))
+    print('Accuracy:', sess.run(tf.reduce_mean(acc)))
